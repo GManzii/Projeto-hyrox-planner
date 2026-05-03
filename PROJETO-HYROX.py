95b215253febbd292c7c75f5406691e3d6d140a9
Manzii__ <gmsca@cesar.school>
2026-05-03 17:02:51 -0300

    Add function to input training details
    Implement add function to collect training data.

--- PROJETO-HYROX.py
+++ PROJETO-HYROX.py
@@ -1,1 +1,17 @@
+treinos = []
+
+def add():
+    tipo = input("Tipo de treino: ")
+    duracao = input("Duração: ")
+    intensidade = input("Intensidade: ")
+    data = input("Data: ")
+    treino = [tipo,duracao,intensidade,data]
+    treinos.append()
+    return add
+
 print("============= Hyrox Planner ============= ")
+
+while True:
+    print("1-adicionar\n2-visualizar\n3-editar\n4-excluir")
+    op = int(input("Escolha: "))
+    if op == 1:
