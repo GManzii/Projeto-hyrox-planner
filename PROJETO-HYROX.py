c4000586a11e00d735c0d5d5a1270bb5fe84494a
annabetelisboa <annaelizabeteasfor@gmail.com>
2026-05-05 11:15:29 -0300

    Update PROJETO-HYROX.py

--- PROJETO-HYROX.py
+++ PROJETO-HYROX.py
@@ -15,7 +15,7 @@ def add():
 
 
 print("============= Hyrox Planner ============= ")
-print("1-adicionar\n2-visualizar\n3-editar\n4-excluir")
+print("1-adicionar\n2-visualizar\n3-editar\n4-excluir\n5-excluir")
 while True:
     
     op = int(input("Escolha: "))
@@ -33,3 +33,34 @@ while True:
                 print("-----")
         else:
             print("Nenhum treino cadastrado")
+    elif op==3:
+        if len(treinos) == 0:
+            print("Nenhum treino cadastrado")
+        else:
+            for i in range(len(treinos)):
+                print(f"{i} - {treinos[i]}")
+
+        indice = int(input("Qual treino deseja editar: "))
+
+        print("0-tipo\n1-duração\n2-intensidade\n3-data")
+        campo = int(input("Digite o campo: "))
+
+        novo_valor = input("Novo valor: ")
+
+        if campo == 0:
+            treinos[indice] = novo_valor
+        elif campo == 1:
+            duracoes[indice] = novo_valor
+        elif campo == 2:
+            intensidades[indice] = novo_valor
+        elif campo == 3:
+            datas[indice] = novo_valor
+        else:
+            print("Campo inválido")
+    elif op==4:
+        valor=input()
+        treinos.remove(valor)
+    elif op==5:
+        break
+    else:
+        print("opção inválida")
