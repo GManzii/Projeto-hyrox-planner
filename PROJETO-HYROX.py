1a5a94f508eb4c647ec095d5f93d903a942774dc
annabetelisboa <annaelizabeteasfor@gmail.com>
2026-05-05 12:02:38 -0300

    Update PROJETO-HYROX.py

--- PROJETO-HYROX.py
+++ PROJETO-HYROX.py
@@ -18,9 +18,25 @@ def vizualizar():
             print(f"duração: {treino['duracao']}")
             print(f"intensidade: {treino['intensidade']}")
             print(f"data: {treino['data']}")
+def editar():
+    i=int(input("qual treino você deseja editar: "))
+    print("tipo/duração/intensidade/data")
+    campo=(input("qual campo deseja editar: "))
+
+    if campo in treinos[i]:
+        novo_valor=input("novo valor: ")
+        treinos[i][campo]=novo_valor
+    else:
+        print("opção inválida")
+def remover():
+    if not treinos:
+        print("treino não encontrado")
+    else:
+        t=int(input("qual treino você deseja remover: "))
+        treinos.pop(t)
 
 print("============= Hyrox Planner ============= ")
-print("1-adicionar\n2-visualizar\n3-editar\n4-excluir\n5-desligar")
+print("1-adicionar\n2-visualizar\n3-editar\n4-excluir\n5-parar")
 while True:
     
     op = int(input("Escolha: "))
@@ -28,3 +44,9 @@ while True:
         add()
     elif op == 2:
         vizualizar()
+    elif op == 3:
+        editar()
+    elif op == 4:
+        remover()
+    elif op ==5:
+        break
