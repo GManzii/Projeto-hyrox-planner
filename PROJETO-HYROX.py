4a63244bb199b1b9461842ab7fa660fd8909d1d9
Manzii.Hacker__ <gmsca@cesar.school>
2026-05-05 09:53:17 -0300

    Refactor treino storage and display logic
    Refactor add function to store treino details in separate lists for duration, intensity, and date. Update main loop to display treino details.

--- PROJETO-HYROX.py
+++ PROJETO-HYROX.py
@@ -1,18 +1,33 @@
 treinos = []
+duracoes = []
+intensidades = []
+datas = []
 
 def add():
     tipo = input("Tipo de treino: ")
+    treinos.append(tipo)
     duracao = input("Duração: ")
+    duracoes.append(duracao)
     intensidade = input("Intensidade: ")
+    intensidades.append(intensidade)
     data = input("Data: ")
-    treino = [tipo,duracao,intensidade,data]
-    treinos.append()
-    return add
+    datas.append(data)
+
 
 print("============= Hyrox Planner ============= ")
-
+print("1-adicionar\n2-visualizar\n3-editar\n4-excluir")
 while True:
-    print("1-adicionar\n2-visualizar\n3-editar\n4-excluir")
+    
     op = int(input("Escolha: "))
     if op == 1:
         add()
+    elif op == 2:
+        if len(treinos) > 0:
+            for i in range(len(treinos)):
+                print(f"Treino: {treinos[i]}")
+                print(f"Duração: {duracoes[i]}")
+                print(f"Intensidade: {intensidades[i]}")
+                print(f"Data: {datas[i]}")
+                print("-----")
+        else:
+            print("Nenhum treino cadastrado")
