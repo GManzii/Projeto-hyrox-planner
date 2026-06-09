963a9f598b47a46f6ba366757e2b7faf4142bb33
Isabela Melo da Silva <isamelodasilva20@gmail.com>
2026-06-08 22:06:31 -0300

    Update PROJETO-HYROX.py

--- PROJETO-HYROX.py
+++ PROJETO-HYROX.py
@@ -498,8 +498,6 @@ def sugestoes():
     nivel = input("Insira o seu nível: (iniciante; intermediário; avançado ):\n").strip().lower()
 
     #aceita tanto com acento quanto sem acento
-    if nivel == "iniciante":
-        nivel = "iniciante"
     if nivel == "intermediário":
         nivel = "intermediario"
     if nivel == "avançado":
